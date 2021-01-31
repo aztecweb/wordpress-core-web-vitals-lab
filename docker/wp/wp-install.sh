1f21dbb3f120ed6b97d1a59f146a5045ff19ccd3
Eduardo Pittol <edpittol@gmail.com>
2021-01-30 22:58:56 -0300

    Update instalation script
    - Always overwrite the config file
    - Allow install themes and plugins by interface

--- docker/wp/wp-install.sh
+++ docker/wp/wp-install.sh
@@ -2,6 +2,8 @@
 #
 # Install a WordPress application from scratch
 
+CONFIG_FILE=wp-config.php
+
 # Download a fresh version of WordPress. It will be skipped if already exists the WordPress files into the public
 # directory
 echo 'Downloading WordPress'
@@ -10,6 +12,7 @@ wp --allow-root core download
 # Create the wp-config.php inner public directory if it doesn't exist
 # The extra code is to allow serve the WordPress agnostically
 echo 'Creating wp-config.php file'
+[ -f ${CONFIG_FILE} ] && echo 'Removing existent file' && rm ${CONFIG_FILE}
 wp --allow-root config create --dbhost=db --dbname=db --dbuser=root --dbpass=db --extra-php <<PHP
 /**
  * Set WordPress home and site URLs
@@ -25,9 +28,15 @@ if( in_array( \$port, array( 443, 44380 ) ) ) {
     \$scheme = 'https';
 }
 
-define('WP_HOME', "{\$scheme}://{\$_SERVER['HTTP_HOST']}/");
-define('WP_SITEURL', "{\$scheme}://{\$_SERVER['HTTP_HOST']}/");
+define( 'WP_HOME', "{\$scheme}://{\$_SERVER['HTTP_HOST']}/" );
+define( 'WP_SITEURL', "{\$scheme}://{\$_SERVER['HTTP_HOST']}/" );
+
+// Allow WordPress right on FS directly
+define( 'FS_METHOD', 'direct' );
 PHP
+
+# Allow everybidy write con wp-content. Never do that on production!
+chmod 777 -R wp-content
 
 # Create the installation data on database if it doesn't exist yet
 echo 'Installing WordPress'
